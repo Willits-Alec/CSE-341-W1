5d6cd57c6e4285ec4c3bf3c4ad28be93746c869e
Alec Willits <alecwillits@gmail.com>
2024-06-22 20:37:14 -0600

    remade swagger file and checked http and https for cores

--- swagger.js
+++ swagger.js
@@ -4,19 +4,13 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'My API',
-    description: 'Description',
+    description: 'API documentation',
   },
   host: 'localhost:3000',
   schemes: ['http', 'https'],
 };
 
 const outputFile = './swagger-output.json';
-const endpointsFiles = ['./server.js', './routes/routes.js']; 
+const endpointsFiles = ['./server.js', './routes/routes.js'];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
-
-
-
-
-
-const endpointFiles = ['/routes/routes.js']
