30d52d68312e5aa4c95180db3a6c97fc2beb0c01
Alec Willits <alecwillits@gmail.com>
2024-06-22 21:05:05 -0600

    require cors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger-output.json');
+const cors = require('cors');
 const app = express();
 
 dotenv.config();
